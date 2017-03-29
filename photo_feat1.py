01b3fd7f2523f22b95a39de48b260db57c1588dc
Antoine Movschin <antoine.movschin@gmail.com>
2017-03-29 07:56:25 +0200

    modif photo_feat1

--- photo_feat1.py
+++ photo_feat1.py
@@ -13,73 +13,111 @@ import pandas as pd
 
 
 def get_photo_features(df):
+    nrows = df.shape[0]
     features = {}
-    features['size'] = []
-    features['mini'] = []
-    features['maxi'] = []
-    features['mean'] = []
-    features['std'] = []
-    features['median'] = []
-    features['percent25'] = []
-    features['percent75'] = []
-    for n in range(df.shape[0]):
+    features['size'] = np.zeros((nrows,))
+    features['mini'] = np.zeros((nrows,))
+    features['maxi'] = np.zeros((nrows,))
+    features['mean'] = np.zeros((nrows,))
+    features['std'] = np.zeros((nrows,))
+    features['median'] = np.zeros((nrows,))
+    features['percent25'] = np.zeros((nrows,))
+    features['percent75'] = np.zeros((nrows,))
+    
+    err_list = np.zeros((nrows,))
+
+    #for n in range(df.shape[0]):
+    for n in range(nrows):
         if n % 100 == 0:
             print ('processing photos ... ' + str(n) + ' / ' + str(df.shape[0]))
         # list of photo URLs
         photos = df['photos'].values[n]
+        nphotos = len(photos)
         # if there are no photos, all values are set to 0
-        if len(photos) == 0:
+        if nphotos == 0:
             for k in features.keys():
-                features[k].append(0)
+                features[k][n] = 0
             continue
-          
-        size = []
-        mini = []
-        maxi = []
-        mean = []
-        std = []
-        median = []
-        percent25 = []
-        percent75 = []        
-        for p in range(len(photos)):
+        
+        size = np.zeros((nphotos,))
+        mini = np.zeros((nphotos,))
+        maxi = np.zeros((nphotos,))
+        mean = np.zeros((nphotos,))
+        std = np.zeros((nphotos,))
+        median = np.zeros((nphotos,))
+        percent25 = np.zeros((nphotos,))
+        percent75 = np.zeros((nphotos,))        
+        for p in range(nphotos):
             url = photos[p]
             # get image from URL
-            response = requests.get(url)
-            image = Image.open(StringIO(response.content))    
-            # convert to grayscale
-            image = image.convert(mode = "L")
-            # convert to numpy array
-            img = np.array(image.getdata())
+            try:
+                response = requests.get(url)
+            except ConnectionError:
+                image = None
+                print ("error image " + str(p) + ", n = " + str(n))
+                err_list[n] = p
+            try:
+                image = Image.open(StringIO(response.content))    
+            except IOError:
+                image = None
+                print ("error image " + str(p) + ", n = " + str(n))
+                err_list[n] = p
             
-            # size
-            size.append(len(img))
-            # min value
-            mini.append(min(img))
-            # max value
-            maxi.append(max(img))
-            # mean
-            mean.append(np.mean(img))
-            # standard deviation
-            std.append(np.std(img))
-            # median 
-            median.append(np.median(img))
-            # 1st quartile
-            percent25.append(np.percentile(img, q=25))
-            # 3rd quartile
-            percent75.append(np.percentile(img, q=75))
-            
-            #plt.imshow(img.reshape(np.flipud(image.size)))
+            if image == None:
+                # on remet les caractéristiques de la dernière image valide
+                # TODO: modifier (on peut faire mieux)
+                # size
+                size[p] = len(img)
+                # min value
+                mini[p] = min(img)
+                # max value
+                maxi[p] = max(img)
+                # mean
+                mean[p] = np.mean(img)
+                # standard deviation
+                std[p] = np.std(img)
+                # median 
+                median[p] = np.median(img)
+                # 1st quartile
+                percent25[p] = np.percentile(img, q=25)
+                # 3rd quartile
+                percent75[p] = np.percentile(img, q=75)
+                
+            else:
+                # convert to grayscale
+                image = image.convert(mode = "L")
+                # convert to numpy array
+                img = np.array(image.getdata())
+                
+                # size
+                size[p] = len(img)
+                # min value
+                mini[p] = min(img)
+                # max value
+                maxi[p] = max(img)
+                # mean
+                mean[p] = np.mean(img)
+                # standard deviation
+                std[p] = np.std(img)
+                # median 
+                median[p] = np.median(img)
+                # 1st quartile
+                percent25[p] = np.percentile(img, q=25)
+                # 3rd quartile
+                percent75[p] = np.percentile(img, q=75)
+                
+                #plt.imshow(img.reshape(np.flipud(image.size)))
         
-        features['size'].append(np.mean(size))
-        features['mini'].append(np.mean(mini))
-        features['maxi'].append(np.mean(maxi))
-        features['mean'].append(np.mean(mean))
-        features['std'].append(np.mean(std))
-        features['median'].append(np.mean(median))
-        features['percent25'].append(np.mean(percent25))
-        features['percent75'].append(np.mean(percent75))
+        features['size'][n] = np.mean(size)
+        features['mini'][n] = np.mean(mini)
+        features['maxi'][n] = np.mean(maxi)
+        features['mean'][n] = np.mean(mean)
+        features['std'][n] = np.mean(std)
+        features['median'][n] = np.mean(median)
+        features['percent25'][n] = np.mean(percent25)
+        features['percent75'][n] = np.mean(percent75)
     
-    return features
+    return features, err_list
 
 
 def add_photo_features(df):
@@ -99,14 +137,15 @@ def add_photo_features(df):
 
 if __name__ == '__main__':
     directory = '../input/photo_features/'
-    date = mp.strdate(format = '_YYYYMMDD')
 
     train_df = pd.read_json(open(data_path + "train.json", "r"))
     train_features = get_photo_features(train_df)
+    date = mp.strdate()
     train_features.to_csv(directory + "train_photo_features" + date + ".csv", index=False)
 
     test_df = pd.read_json(open(data_path + "test.json", "r"))
     test_features = get_photo_features(train_df)
+    date = mp.strdate()
     test_features.to_csv(directory + "test_photo_features" + date + ".csv", index=False)
 
         

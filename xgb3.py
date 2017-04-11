7215c89b7dc3aefd743afbea86c5f6ef403588ea
Antoine Movschin <antoine.movschin@gmail.com>
2017-04-11 20:44:10 +0200

    improve linreg

--- xgb3.py
+++ xgb3.py
@@ -252,6 +252,17 @@ def runXGB(train_X, train_y, test_X, test_y=None, feature_names=None, seed_val=0
     
     return pred_test_y, model
 
+def saturate_listing_id_created(pred_df):
+    borne_1 = 200000
+    borne_2 = 250000
+    pred_df['alpha'] = pred_df['d'].apply(lambda x: alpha_listing_id_created(x))
+    pred_df['alpha'] = 0
+    pred_df.loc[pred_df['d'] >= borne_1, 'alpha'] = 1.0*(pred_df.loc[pred_df['d'] >= borne_1, 'd']-borne_1)/(borne_2-borne_1)
+    pred_df.loc[pred_df['d'] >= borne_2, 'alpha'] = 1
+    pred_df[0] = pred_df['alpha'] + (1-pred_df['alpha']) * pred_df[0]
+    pred_df[1] = (1-pred_df['alpha']) * pred_df[1]
+    pred_df[2] = (1-pred_df['alpha']) * pred_df[2]
+    del pred_df['alpha']
 
 def predictXGB(train_df, test_df, train_X, train_y, test_X, test_y=None, feature_names=None, seed_val=0, num_rounds=2000, verbose=True):
     
@@ -269,9 +280,7 @@ def predictXGB(train_df, test_df, train_X, train_y, test_X, test_y=None, feature
     
     preds = pd.DataFrame(pred_test_y)
     preds['d'] = d
-    preds.loc[preds['d'] > 250000, 0] = 1
-    preds.loc[preds['d'] > 250000, 1] = 0
-    preds.loc[preds['d'] > 250000, 2] = 0
+    saturate_listing_id_created(preds)
     del preds['d']
     pred_test_y = preds.values
 

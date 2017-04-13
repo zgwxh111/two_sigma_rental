56116bd2625f904def89c0963b2f4f5aec2e1730
Antoine Movschin <antoine.movschin@gmail.com>
2017-04-13 21:11:54 +0200

    fix param tuning

--- param_tuning.py
+++ param_tuning.py
@@ -9,27 +9,15 @@ Created on Thu Apr 13 20:02:42 2017
 from xgb3 import crossval
 
 params={}
-params['objective'] = 'multi:softprob'
 params['eta'] = [0.01, 0.02, 0.03, 0.05, 0.08, 0.1, 0.2] 
 params['max_depth'] = [3,4,5]
-params['silent'] = [1]
-params['num_class'] = [3]
-params['eval_metric'] = "mlogloss"
 params['min_child_weight'] = [1, 2, 3]
-params['subsample'] = [0.7]
-params['colsample_bytree'] = [0.7]
 params['gamma'] = [0.1, 0.2, 0.5]
-params['seed'] = 0
 num_rounds = 4000
-
-plst = list(params.items())
 
 
 result=pd.DataFrame([])
 
-xgtrain = xgb.DMatrix(X_train, label=y_train)
-
-keys = params.keys()
 for eta in params['eta']:
     for max_depth in params['max_depth']:
         for min_child_weight in params['min_child_weight']:
@@ -47,7 +35,7 @@ for eta in params['eta']:
                 param['gamma'] = gamma
                 param['seed'] = 0
                 
-                res = pd.DataFrame
+                res = pd.DataFrame()
                 res['eta'] = [eta]
                 res['max_depth'] = [max_depth]
                 res['min_child_weight'] = [min_child_weight]
